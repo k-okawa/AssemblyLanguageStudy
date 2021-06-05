b8c001cc8de06d09daf1fe4b1c660fc608ad735f
KyoheiOkawa <kurokenken0310@gmail.com>
2021-06-05 22:21:13 +0900

    オプション追加

--- work/helloworld/hello.s
+++ work/helloworld/hello.s
@@ -3,26 +3,22 @@
 	.section	.rodata
 .LC0:
 	.string	"HelloWorld"
+.LC1:
+	.string	"HelloWorld2"
 	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
-	.cfi_startproc
 	endbr64
 	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
 	leaq	.LC0(%rip), %rdi
+	call	puts@PLT
+	leaq	.LC1(%rip), %rdi
 	call	puts@PLT
 	movl	$0, %eax
 	popq	%rbp
-	.cfi_def_cfa 7, 8
 	ret
-	.cfi_endproc
-.LFE0:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
 	.section	.note.GNU-stack,"",@progbits
